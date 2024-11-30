d543f46cb5344892551fa924911dfbf07a0a1b5a
SebMed21 <sebastian.medina301426@gmail.com>
2024-11-30 22:59:00 +0800

    added a way to search the txt file and for the user to input a name

--- personal_info_txt_read_main.py
+++ personal_info_txt_read_main.py
@@ -1,27 +1,41 @@
 
 #once again for the sake of simplicity and convinience I am using an array for the retry
 retry_array = ["Y," "N"]
-
-#opens the txt file
-with open("output.txt", "r") as output:
-   for line in output:
-       line = line.strip()
-       
        
 while True:
-    while True:
+    #prompt the user for a name to search in the text file
+    name_search = input("Please enter the name you are looking for here: ").strip()
+    
+    #open the text file
+    with open("output.txt", "r") as output:
+        #this reads all the lines
+        lines = output.read()
+        for i, line in enumerate(lines):
+            # check if the name matches in lines that start with "Name:"
+            if "Name:" in line and name_search.lower() in line.lower():
+                #prints the name when found
+                print(f"\nRecord found:\n{line.strip()}")
+                #prints the birthday and age after the name
+                print(lines[i + 1].strip())
+                print(lines[i + 2].strip())
+                break
         
-        #asks the user for a name to search in the txt file
-        name_search = input("please enter the name you are looking for here: ").strip().lower()
-        
-         #this is what progresses the loop
-        while not retry_input in retry_array:
-            retry_input = input("Would you like to try more? ( Y / N ) : ")
+        #if there is no no name found it will print this 
+        else:
+            print("Name not found. Please try again.")
+            
+            
+                
+        #this is what progresses the loop
+      #  while not retry_input in retry_array:
+       #     retry_input = input("Would you like to try more? ( Y / N ) : ")
             
         #if the user answers "N" the loop will stop
-        if retry_input in retry_array[1]: 
-            break
+       # if retry_input in retry_array[1]: 
+        #    break
             
-    break
+#    break
+
+
         
         
